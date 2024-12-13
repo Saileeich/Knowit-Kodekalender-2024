1b4ab79c95ce8d08c6897ef31b2cc55617ac5fe2
Saileeich <109689239+Saileeich@users.noreply.github.com>
2024-12-13 21:04:00 +0100

    Fullført luke 3

--- Luke3 - Pytt i julepanne/main.py
+++ Luke3 - Pytt i julepanne/main.py
@@ -1,48 +1,76 @@
-class Nissmor:
-    def __init__(self):
-        self.påfyll = {
-            "ris": [0,0,1,0,0,2],
-            "erter": [0,3,0,0],
-            "gulrøtter": [0,1,0,0,0,8],
-            "reinsdyrkjøtt": [100,80,40,10]
-        }
-        self.kjøtt_tid = 0
-        self.kjøtt_index = 0
+tallerken = {
+    "ris": 100,
+    "erter": 100,
+    "gulrøtter": 100,
+    "reinsdyrkjøtt": 100,
+    "julekringle": 100
+}
 
-    def get_looped_index(self, t, l):
-        return t - len(l) * (t // len(l))
-    
-    def fyll_på(self, tallerken, t):
-        return
+påfyll = {
+    "ris": [0, 0, 1, 0, 0, 2],
+    "erter": [0, 3, 0, 0],
+    "gulrøtter": [0, 1, 0, 0, 0, 8],
+    "reinsdyrkjøtt": [100, 80, 40, 20, 10]
+}
 
-
-
-class Alvefine:
-    def __init__(self):
-        self.tallerken = {
-            "ris": 100,
-            "erter": 100,
-            "gulrøtter": 100,
-            "reinsdyrkjøtt": 100,
-            "julekringle": 100
-        }
-
-    def zero_subtract(self, a, b):
-        return max(a-b, 0)
-
-    def spis(self, t):
-        for rett in self.tallerken.values():
-            print(rett)
+def spis_mat(tallerken: dict) -> list:
+    spist_mat = []
+    for mat in list(tallerken.keys())[:3:]:
+        if mat in spist_mat:
+            continue
+        
+        if tallerken[mat] > 0:
+            spist_mat.append(mat)
+            tallerken[mat] = max(tallerken[mat] - 5, 0)
             break
-
-
+    
+    if not spist_mat:
+        if tallerken["reinsdyrkjøtt"] > 0:
+            spist_mat.append("reinsdyrkjøtt")
+            tallerken["reinsdyrkjøtt"] = max(tallerken["reinsdyrkjøtt"] - 2, 0)
+        else:
+            spist_mat.append("julekringle")
+            tallerken["julekringle"] = max(tallerken["julekringle"] - 1, 0)
+    else:
+        for mat in list(tallerken.keys())[1:3:]:
+            if mat in spist_mat:
+                continue
             
-alvefine = Alvefine()
-nissmor = Nissmor()
+            if tallerken[mat] > 0:
+                spist_mat.append(mat)
+                tallerken[mat] = max(tallerken[mat] - 3, 0)
+                break
+    return spist_mat
 
-t = -1
-while alvefine.tallerken["julekringle"] > 0:
+reinsdyrkjøtt_tid = 0
+def fyll_på(tallerken: dict, påfyll: dict, t: int, reinsdyrkjøtt_tid: int) -> list:
+    ris_påfyll = påfyll["ris"][t % len(påfyll["ris"])]
+    erter_påfyll = påfyll["erter"][t % len(påfyll["erter"])]
+    gulrot_påfyll = påfyll["gulrøtter"][(t-31) % len(påfyll["gulrøtter"])] if t > 30 else 0
+    fylt_på = []
+    
+    tallerken["ris"] += ris_påfyll
+    tallerken["erter"] += erter_påfyll
+    tallerken["gulrøtter"] += gulrot_påfyll
+
+    if ris_påfyll: fylt_på.append("ris")
+    if erter_påfyll: fylt_på.append("erter")
+    if gulrot_påfyll: fylt_på.append("gulrøtter")
+
+    if not tallerken["reinsdyrkjøtt"] and reinsdyrkjøtt_tid >= 0 and påfyll["reinsdyrkjøtt"]:
+        tallerken["reinsdyrkjøtt"] += påfyll["reinsdyrkjøtt"].pop(0)
+        reinsdyrkjøtt_tid = -50
+        fylt_på.append("reinsdyrkjøtt")
+    
+    return fylt_på
+    
+
+
+t = 0
+while tallerken["julekringle"]:
+    spis_resultat = spis_mat(tallerken)
+    fyll_resultat = fyll_på(tallerken, påfyll, t, reinsdyrkjøtt_tid)
+    print(f"{t:<5} {str(spis_resultat):<20} {str(fyll_resultat):<48} {tallerken}")
+    reinsdyrkjøtt_tid += 1
     t += 1
-    alvefine.spis(t)
-    nissmor.fyll_på(alvefine.tallerken, t)
 print(t)
